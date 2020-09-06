622aeecf213becdfbc6ccdfedb4709f3404b267f
Billyyanyz <yanyz18@mails.tsinghua.edu.cn>
2020-09-06 16:25:43 +0800

    Add two Subthread (unfinished) and one abstracted news object

---
+++ app/src/main/java/com/java/liuyun/NewsAbstractObject.java
@@ -0,0 +1,85 @@
+package com.java.liuyun;
+
+import org.json.*;
+import org.litepal.crud.LitePalSupport;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class NewsAbstractObject extends LitePalSupport {
+
+    String newsID;
+    String type;
+    String title;
+    Date publishTime;
+    NewsObject newsObject = null;
+
+    public String getNewsID() {
+        return newsID;
+    }
+
+    public void setNewsID(String newsID) {
+        this.newsID = newsID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(Date publishTime) {
+        this.publishTime = publishTime;
+    }
+
+    public NewsAbstractObject()
+    {
+        super();
+
+    }
+
+    void parseJSON(JSONObject jsonData)
+    {
+        try{
+            newsID = jsonData.getString("_id");
+        }catch (Exception e){
+            newsID = "";
+        }
+
+        try{
+            type = jsonData.getString("type");
+        }catch (Exception e){
+            type = "";
+        }
+
+        try{
+            title = jsonData.getString("title");
+        }catch (Exception e){
+            title = "";
+        }
+
+        try{
+            String timeStr = jsonData.getString("time");
+            SimpleDateFormat ft = new SimpleDateFormat ("yyyy/MM/dd", Locale.CHINA);
+            publishTime = ft.parse(timeStr);
+        }catch (Exception e){
+            publishTime = null;
+        }
+
+    }
+
+}
--- app/src/main/java/com/java/liuyun/NewsObject.java
+++ app/src/main/java/com/java/liuyun/NewsObject.java
@@ -1,21 +1,103 @@
 package com.java.liuyun;
 
 import org.json.*;
+import org.litepal.crud.LitePalSupport;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class NewsObject {
+public class NewsObject extends LitePalSupport {
+
     String newsID;
     String type;
     String title;
     String content;
-    String category;
     Date publishTime;
     String language;
     String source;
     String newsURL;
     HashMap<String, String> entityMap;
+
+    public String getNewsID() {
+        return newsID;
+    }
+
+    public void setNewsID(String newsID) {
+        this.newsID = newsID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(Date publishTime) {
+        this.publishTime = publishTime;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getNewsURL() {
+        return newsURL;
+    }
+
+    public void setNewsURL(String newsURL) {
+        this.newsURL = newsURL;
+    }
+
+    public HashMap<String, String> getEntityMap() {
+        return entityMap;
+    }
+
+    public void setEntityMap(HashMap<String, String> entityMap) {
+        this.entityMap = entityMap;
+    }
+
+    public ArrayList<String> getRelatedNewsID() {
+        return relatedNewsID;
+    }
+
+    public void setRelatedNewsID(ArrayList<String> relatedNewsID) {
+        this.relatedNewsID = relatedNewsID;
+    }
+
     ArrayList<String> relatedNewsID;
 
     public NewsObject()
@@ -50,14 +132,8 @@ public class NewsObject {
         }
 
         try{
-            category = jsonData.getString("category");
-        }catch (Exception e){
-            category = "";
-        }
-
-        try{
-            String timeStr = jsonData.getString("publishTime");
-            SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+            String timeStr = jsonData.getString("time");
+            SimpleDateFormat ft = new SimpleDateFormat ("yyyy/MM/dd ", Locale.CHINA);
             publishTime = ft.parse(timeStr);
         }catch (Exception e){
             publishTime = null;
