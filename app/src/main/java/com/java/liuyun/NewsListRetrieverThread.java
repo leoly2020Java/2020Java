283849df01ba5e023acb2fcea08a34b814f8f8af
Billyyanyz <yanyz18@mails.tsinghua.edu.cn>
2020-09-06 19:23:12 +0800

    Update NewsListRetrieverThread.java

--- app/src/main/java/com/java/liuyun/NewsListRetrieverThread.java
+++ app/src/main/java/com/java/liuyun/NewsListRetrieverThread.java
@@ -4,6 +4,8 @@ import android.os.Message;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -14,11 +16,11 @@ import java.util.Date;
 
 public class NewsListRetrieverThread extends Thread{
 
-    public void getAllNews()
+    public void getNews()
     {
-        boolean moreNews = true;
         try{
-            String urlHeader = "https://covid-dashboard.aminer.cn/api/events/list?size=20&page=";
+            boolean moreNews = true;
+            String urlHeader = "https://covid-dashboard.aminer.cn/api/events/list?page=";
             int page = 1;
             while(moreNews)
             {
@@ -55,11 +57,47 @@ public class NewsListRetrieverThread extends Thread{
         }catch(Exception ignored){}
     }
 
-    public void addLatestNews(Date currentLatest)
+    public void getNews(Date currentLatest)
     {
-        
-    }
-    {
-
+        try{
+            boolean moreNews = true;
+            String urlHeader = "https://covid-dashboard.aminer.cn/api/events/list?page=";
+            int page = 1;
+            while(moreNews)
+            {
+                moreNews = false;
+                URL url = new URL(urlHeader + page);
+                page++;
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder stringBuffer = new StringBuilder();
+                String line;
+                while((line = bufferedReader.readLine()) != null)
+                {
+                    stringBuffer.append(line);
+                }
+                bufferedReader.close();
+                inputStreamReader.close();
+                inputStream.close();
+                JSONObject json = new JSONObject(stringBuffer.toString());
+                JSONArray jsonArray = json.getJSONArray("data");
+                if (jsonArray.length() > 0)
+                {
+                    moreNews = true;
+                    for (int i = 0; i < jsonArray.length(); i++)
+                    {
+                        JSONObject jsonNews = jsonArray.getJSONObject(i);
+                        NewsAbstractObject newsAbstractObject = new NewsAbstractObject();
+                        newsAbstractObject.parseJSON(jsonNews);
+                        if (newsAbstractObject.getPublishTime().after(currentLatest))
+                        {
+                            newsAbstractObject.save();
+                        }
+                    }
+                }
+            }
+        }catch(Exception ignored){}
     }
 }
