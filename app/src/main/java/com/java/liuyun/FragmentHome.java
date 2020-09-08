ab4529839b962704cb7aba338afd043d6cbe99a0
liuyun <liuyun18@mails.tsinghua.edu.cn>
2020-09-08 12:25:24 +0800

    debug FragmentHome.java

--- app/src/main/java/com/java/liuyun/FragmentHome.java
+++ app/src/main/java/com/java/liuyun/FragmentHome.java
@@ -1,14 +1,19 @@
 package com.java.liuyun;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.lcodecore.tkrefreshlayout.RefreshListenerAdapter;
 import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
 
 import org.litepal.LitePal;
@@ -65,6 +70,7 @@ public class FragmentHome extends Fragment {
     public void flush() {
         int pageSize = 12;
         newsList.clear();
+        /*
         NewsListUpdaterThread newsListUpdaterThread = new NewsListUpdaterThread();
         newsListUpdaterThread.start();
         try{
@@ -84,10 +90,16 @@ public class FragmentHome extends Fragment {
                         .order("publishTime desc").limit(pageSize - newsList.size()).find(NewsAbstractObject.class));
             }catch(Exception ignored){}
         }
+         */
+
+        Toast.makeText(getActivity(), "flush: newsList.size() = "+newsList.size(), Toast.LENGTH_SHORT).show(); //Debug
+
     }
 
     public void loadMore() {
         int pageSize = 12;
+
+        /*
         long earliestInList = newsList.get(newsList.size() - 1).getPublishTime().getTime();
         newsList.addAll(LitePal.where("publishTime < ? and title like ? and type in (?)", Long.toString(earliestInList), "%" + keyWord + "%", "\"" + String.join("\",\"", categoryItemList) + "\"")
                 .limit(pageSize).find(NewsAbstractObject.class));
@@ -104,16 +116,62 @@ public class FragmentHome extends Fragment {
                 newsList.sort(new SortByTimeDesc());
             }catch(Exception ignored){}
         }
+         */
+
+        Toast.makeText(getActivity(), "loadMore: newsList.size() = "+newsList.size(), Toast.LENGTH_SHORT).show(); //Debug
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_home, container, false);
         twinklingRefreshLayout = (TwinklingRefreshLayout) view.findViewById(R.id.fragment_home_layout);
+
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        LinearLayoutManager llm =new LinearLayoutManager((MainActivity)getActivity());
+        llm.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+        recyclerView.addItemDecoration(new DividerItemDecoration((MainActivity)getActivity(),RecyclerView.VERTICAL));
+
+        //TODO:adapter = new NewsRecyclerViewAdapter(getActivity(),newsList);
+        //TODO:recyclerView.setAdapter(adapter);
 
         twinklingRefreshLayout.startRefresh();
         //实现setOnRefreshListener
+        twinklingRefreshLayout.setOnRefreshListener(new RefreshListenerAdapter() {
+            //下拉刷新最新新闻
+            @Override
+            public void onRefresh(final TwinklingRefreshLayout twinklingRefreshLayout) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        flush(); //重新加载新闻
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {}
+                        //TODO:adapter.notifyDataSetchanged();
+                        twinklingRefreshLayout.finishRefreshing();
+                    }
+                }, 1000);
+            }
+
+            //上拉获取更多新闻
+            @Override
+            public void onLoadMore(final TwinklingRefreshLayout twinklingRefreshLayout) {
+                Toast.makeText(getActivity(), "onLoadMore", Toast.LENGTH_SHORT).show(); //Debug
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadMore(); //获取更多新闻
+                        try {
+                            Thread.sleep(1000);
+                        }catch (Exception e){}
+                        //TODO:adapter.notifyDataSetChanged();
+                        twinklingRefreshLayout.finishLoadmore();
+                    }
+                }, 1000);
+            }
+        });
 
         return view;
     }
